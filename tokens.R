81bfd1053d4f153ffb4df724ae3656ca2f536ec4
Demps <idemps@gmail.com>
2016-11-02 22:31:08 +0000

    Complete v1

---
+++ tokens.R
@@ -0,0 +1,14 @@
+prac 2 work space: cKmLze3dMDHvcSJn - done
+prac 3 sequences: menLGzQKlaosTRPa
+prac 4 vectors: fkt7Du3e3eri8vnJ
+prac 5 Nas: MprONxXMu1gsmSpk - done
+prac 6 sub sets: L2P4rzxPiPMoHgBf - done
+prac 7 matrices: I6P3YR4D7mxfpZzk - done
+
+
+Re-do Function, Subsetting, Vectors
+
+http://www.ling.upenn.edu/~joseff/rstudy/week4.html - base graphics
+
+http://varianceexplained.org/RData/ - alt R tutorials
+
